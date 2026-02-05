c974840cec4c2ca79b1eff86ef2e842e9b9dea27
PAIDALABS <165406748+PAIDALABS@users.noreply.github.com>
2026-02-06 01:55:26 +0530

    Add version number (v11) to bottom right corner

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'nfc-card-v10';
+var CACHE = 'nfc-card-v11';
 var ASSETS = ['admin.html','icon-192.png','icon-512.png','apple-touch-icon.png'];
 
 self.addEventListener('install', function(e) {
